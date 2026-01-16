261cbbc4ae56eff416fadf2e00fb9d7ad94b2d30
pushkar raj <pushkarraj192003@gmail.com>
2026-01-16 12:22:19 +0530

    conected postgres

--- secureVault/secureVault/settings.py
+++ secureVault/secureVault/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'secureVault.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'secureVault',
+        'USER': 'postgres',
+        'PASSWORD':'192003',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
-}
+
+    }
 
 
 # Password validation
